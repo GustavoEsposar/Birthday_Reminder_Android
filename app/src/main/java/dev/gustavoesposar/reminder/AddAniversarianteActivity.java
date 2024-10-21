962d20b8d17e322975786d36448ef35a6c5d2497
Gustavo Dektop-Ubuntu <guesposar@icloud.com>
2024-10-21 16:30:29 -0300

    Initial commit

---
+++ app/src/main/java/dev/gustavoesposar/reminder/AddAniversarianteActivity.java
@@ -0,0 +1,17 @@
+package dev.gustavoesposar.reminder;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddAniversarianteActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+    }
+}
