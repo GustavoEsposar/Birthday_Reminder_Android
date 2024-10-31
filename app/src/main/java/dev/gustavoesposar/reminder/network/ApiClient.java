e58ccb30df4a85dd491c6d1eee71b5d7e3119b9d
Gustavo Dektop-Ubuntu <guesposar@icloud.com>
2024-10-30 21:03:51 -0300

    BASE_URL corrigida para o servidor

--- app/src/main/java/dev/gustavoesposar/reminder/network/ApiClient.java
+++ app/src/main/java/dev/gustavoesposar/reminder/network/ApiClient.java
@@ -5,9 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
     private static Retrofit retrofit;
-    static String x = "http://10.0.2.2:3000/";
-    static String y = "http://192.168.15.148:3000/";
-    private static final String BASE_URL = y;
+    private static final String BASE_URL = "https://reminder.gustavoesposar.dev/";
 
     public static Retrofit getClient() {
         if (retrofit == null) {
