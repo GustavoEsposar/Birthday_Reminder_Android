919b866f2d1ad5a61072fe30cb2826cea92ae4ba
Gustavo Dektop-Ubuntu <guesposar@icloud.com>
2024-11-01 14:43:26 -0300

    Novos componentes visuais adicionados a fragment_aniversarios.xml

--- app/src/main/java/dev/gustavoesposar/reminder/view/AniversariosFragment.java
+++ app/src/main/java/dev/gustavoesposar/reminder/view/AniversariosFragment.java
@@ -9,6 +9,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -42,6 +44,9 @@ public class AniversariosFragment extends Fragment {
     private RecyclerView recyclerView;
     private AniversarianteAdapter aniversarianteAdapter;
     private List<Aniversariante> aniversariantes = new ArrayList<>();
+    private ProgressBar progressBarLoading;
+    private ImageView imageViewEmpty;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -58,6 +63,9 @@ public class AniversariosFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_aniversarios, container, false);
+
+        progressBarLoading = view.findViewById(R.id.progressBarLoading);
+        imageViewEmpty = view.findViewById(R.id.imageViewEmpty);
 
         swipeRefreshLayout = view.findViewById(R.id.swipeRefreshLayout);
         swipeRefreshLayout.setOnRefreshListener(this::carregarAniversariantesDoServidor);
@@ -81,53 +89,58 @@ public class AniversariosFragment extends Fragment {
             List<Aniversariante> localAniversariantes = aniversarianteDao.getAllAniversariantes();
             aniversariantes.clear();
             aniversariantes.addAll(localAniversariantes);
-            requireActivity().runOnUiThread(() -> aniversarianteAdapter.notifyDataSetChanged());
+
+            requireActivity().runOnUiThread(() -> {
+                aniversarianteAdapter.notifyDataSetChanged();
+
+                if (aniversariantes.isEmpty()) {
+                    imageViewEmpty.setVisibility(View.VISIBLE);
+                } else {
+                    imageViewEmpty.setVisibility(View.GONE);
+                }
+            });
         }).start();
     }
 
-    private void carregarAniversariantesDoServidor() {
-        if (!isAdded()) return;
 
-        SharedPreferences sharedPreferences = getContext().getSharedPreferences("MyAppPrefs", MODE_PRIVATE);
+    private void carregarAniversariantesDoServidor() {
+        SharedPreferences sharedPreferences = requireContext().getSharedPreferences("MyAppPrefs", MODE_PRIVATE);
         String token = sharedPreferences.getString("JWT_TOKEN", null);
 
         if (token != null) {
+            progressBarLoading.setVisibility(View.VISIBLE);
+
             Call<List<Aniversariante>> call = apiService.getBirthdates("Bearer " + token);
 
             call.enqueue(new Callback<List<Aniversariante>>() {
                 @Override
                 public void onResponse(Call<List<Aniversariante>> call, Response<List<Aniversariante>> response) {
-                    if (isAdded() && response.isSuccessful() && response.body() != null) {
+                    if (response.isSuccessful() && response.body() != null) {
                         new Thread(() -> {
                             aniversarianteDao.deleteAll();
                             aniversarianteDao.insertAll(response.body());
-
-                            if (isAdded()) {
-                                requireActivity().runOnUiThread(() -> carregarAniversariantesLocalmente());
-                            }
+                            requireActivity().runOnUiThread(() -> carregarAniversariantesLocalmente());
                         }).start();
                     }
 
-                    if (isAdded()) {
-                        swipeRefreshLayout.setRefreshing(false);
-                    }
+                    progressBarLoading.setVisibility(View.GONE);
+                    swipeRefreshLayout.setRefreshing(false);
                 }
 
                 @Override
                 public void onFailure(Call<List<Aniversariante>> call, Throwable t) {
-                    if (isAdded()) {
-                        Toast.makeText(getContext(), "Erro ao carregar dados", Toast.LENGTH_SHORT).show();
-                        swipeRefreshLayout.setRefreshing(false);
-                    }
+                    Toast.makeText(getContext(), "Erro ao carregar dados", Toast.LENGTH_SHORT).show();
+                    // Oculte o ProgressBar e o SwipeRefreshLayout após o erro
+                    progressBarLoading.setVisibility(View.GONE);
+                    swipeRefreshLayout.setRefreshing(false);
                 }
             });
         } else {
-            if (isAdded()) {
-                Toast.makeText(getContext(), "Token não encontrado. Faça login novamente.", Toast.LENGTH_SHORT).show();
-                swipeRefreshLayout.setRefreshing(false);
-            }
+            Toast.makeText(getContext(), "Token não encontrado. Faça login novamente.", Toast.LENGTH_SHORT).show();
+            swipeRefreshLayout.setRefreshing(false);
         }
     }
+
 
     private void deletarAniversariante(Aniversariante aniversariante) {
         // Recupere o token do SharedPreferences
